e9a2064c54c737c2198751517935dcb4b45c702a
Hecto <45845790+LamaNivelle@users.noreply.github.com>
2023-12-20 13:37:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,16 @@
-const express = require('express');
 const telegraf = require('telegraf');
 const dotenv = require('dotenv');
 
-// Chargement des variables d'environnement
 dotenv.config();
 
-// Création du serveur
-const app = express();
-
-// Configuration du webhook Telegram
-app.post('/webhook', (req, res) => {
-  // Récupération du message
-  const message = req.body.message;
-
-  // Envoi de la date d'aujourd'hui
-  const today = new Date();
-  const date = `Aujourd'hui, nous sommes le ${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}.`;
-
-  // Envoi du message à Telegram
-  telegraf.sendMessage(req.body.chat.id, date);
-
-  // Réponse au webhook
-  res.sendStatus(200);
+const bot = new telegraf.Bot({
+  token: process.env.BOT_TOKEN,
 });
 
-// Démarrage du serveur
-app.listen(3000);
+bot.on('message', async (ctx) => {
+  const date = new Date();
+
+  ctx.reply(`La date d'aujourd'hui est le ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}.`);
+});
+
+bot.start();
